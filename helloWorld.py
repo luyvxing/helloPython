bbb01fc43dbbd40434457e06f10415530033bbba
luyvxing <luyvxing@126.com>
2017-09-15 16:12:31 +0800

    add my fist python doc

---
+++ helloWorld.py
@@ -0,0 +1,19 @@
+print(100+200);
+print('1024 * 768 = ',1024*768);
+print('I \'m \"ok\"!');
+#转义字符 
+print('\\\n\\');
+print(r'\\\n');
+#多行输出时用'''  '''格式
+#命令行模式时要使用''' ...line1
+#				      ...line2
+#					  ...line3'''的格式
+print('''line1
+line2
+line3''');
+#布尔值运算
+print('True and False = ',True and False);
+print('True and True = ',True and True);
+print('True or False = ',True or False);
+print('True or True =', True or True);
+print('False or False =',False or False);
